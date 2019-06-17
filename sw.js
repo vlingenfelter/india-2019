1fe2e7ceb9f319dda6217ebb6417c08c4191497c
Violet <vlingenfelter5@gmail.com>
2019-06-17 23:45:03 +0200

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-64cbc70832ad3a4d0437.js"
+    "url": "webpack-runtime-ab65d443f19c310ec0df.js"
   },
   {
     "url": "app-03631952be5964b1d52c.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3a8ba8c85ded659bcf71cbca1dc73613"
+    "revision": "1646e0bdb5c1ac7167a0049b0336b36f"
   },
   {
     "url": "static/d/604/path---offline-plugin-app-shell-fallback-a-30-c5a-BawJvyh36KKFwbrWPg4a4aYuc8.json"
