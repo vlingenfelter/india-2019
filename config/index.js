a2a151233308a67a3443e6afdcd953db904400e5
Violet <vlingenfelter5@gmail.com>
2019-06-17 13:57:57 +0200

    first commit

---
+++ config/index.js
@@ -0,0 +1,23 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+
+  siteTitle: 'India Dialogue 2019', // Navigation and Site Title
+  siteTitleAlt: 'Climate Change Dialogue', // Alternative Site title for SEO
+  siteTitleShort: 'Dialogue', // short_name for manifest
+  siteHeadline: 'Climate Change Resilience Dialogue 2019', // Headline for schema.org JSONLD
+  siteUrl: 'https://jodie.lekoarts.de', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: '/logos/logo.png', // Used for SEO and manifest
+  siteDescription: 'Summary of 2019 Climate Change Resilience Dialogue to India and Nepal',
+  author: 'Violet Lingenfelter', // Author for schema.org JSONLD
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: '@jodie', // Twitter Username
+  ogSiteName: 'jodie', // Facebook Site Name
+  ogLanguage: 'en_US', // og:language
+  googleAnalyticsID: 'UA-XXXXXX-X',
+
+  // Manifest and Progress color
+  themeColor: '#db7436',
+  backgroundColor: '#3b3c4f',
+}
